c946bd712768ecf6fdd05e8e66abf47e632358d6
Arnav <a.purohit1903@gmail.com>
2026-06-12 23:49:52 +0530

    Added Gemini Model testing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from dotenv import load_dotenv
+
+## Ollama Model Integration
 from langchain_ollama import ChatOllama
 
 load_dotenv()
@@ -8,5 +10,19 @@ llm = ChatOllama(
 )
 
 response = llm.invoke("Hello!")
+
+# print(response.content)
+
+## Google Gemini Model Integration
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0,
+)
+
+response = llm.invoke("Explain AI agents in 2 lines.")
 
 print(response.content)
